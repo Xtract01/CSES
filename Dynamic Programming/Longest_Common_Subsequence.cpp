23e47eebed0a05cd31ea673357c543b0f0abe3a9
Xtract01 <meena.balwir149@gmail.com>
2026-07-13 22:04:14 +0530

    Add solution for Longest Common Subsequence problem

--- Dynamic Programming/Longest_Common_Subsequence.cpp
+++ Dynamic Programming/Longest_Common_Subsequence.cpp
@@ -23,7 +23,35 @@ const double EPS = 1e-9;
 const double PI = acos(-1.0);
 
 void solve() {
-    // Write your solution for one test case here
+    int n , m;
+    cin >> n >> m;
+    vector<int> a(n),b(m);
+    for(int i=0 ; i<n ; i++) cin>>a[i];
+    for(int i=0 ; i<m ; i++) cin>>b[i];
+    vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+    for(int i=1 ; i<=n ; i++){
+        for(int j=1 ; j<=m ; j++){
+            if(a[i-1]==b[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
+            else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+        }
+    }
+    vector<int> res;
+    int i=n , j = m;
+    while(i>0 && j>0){
+        if(a[i-1]==b[j-1]){
+            res.push_back(a[i-1]);
+            i = i-1;
+            j = j-1;
+        }
+        else{
+            if(dp[i-1][j]>dp[i][j-1]) i = i-1;
+            else j = j-1;
+        }
+    }
+    cout<<dp[n][m]<<endl;
+    for(int i=res.size()-1 ; i>=0 ;i-- ){
+        cout<<res[i]<<" ";
+    }
 }
 
 int main() {
